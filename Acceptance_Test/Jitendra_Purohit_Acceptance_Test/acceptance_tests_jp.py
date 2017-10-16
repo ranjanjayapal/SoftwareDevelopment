7567e9cdb46c65bc36ed3064f76b331d046913f7
Ranjan Jayapal <rjayapal@stevens.edu>
2017-10-16 02:56:16 -0400

    Updated For Sprint 2

---
+++ Acceptance_Test/Jitendra_Purohit_Acceptance_Test/acceptance_tests_jp.py
@@ -0,0 +1,45 @@
+import unittest
+from parse import parse_indi,parse_fam, birth_Before_Death_Of_Parents
+import user_stories_jp
+
+class test_Parent_Not_Too_Old(unittest.TestCase):
+    # def test_Parent_Not_Too_Old(self):
+    #     pass_lines = [line.rstrip('\n\r') for line in open("US12_PassFile.ged")]
+    #     fail_lines = [line.rstrip('\n\r') for line in open("US12_FailFile.ged")]
+    #     individuals_pass = parse_indi(pass_lines)
+    #     families_pass = parse_fam(pass_lines)
+    #     self.assertTrue(user_stories_jp.parents_Not_Too_Old(individuals_pass, families_pass))
+    #     individuals_fail = parse_indi(fail_lines)
+    #     families_fail = parse_fam(fail_lines)
+    #     self.assertFalse(user_stories_jp.parents_Not_Too_Old(individuals_fail, families_fail))
+    # def test_Birth_before_deathof_Parents(self):
+    #     pass_lines = [line.rstrip('\n\r') for line in open("US09_PassFile.ged")]
+    #     fail_lines = [line.rstrip('\n\r') for line in open("US09_FailFile.ged")]
+    #     individuals_pass = parse_indi(pass_lines)
+    #     families_pass = parse_fam(pass_lines)
+    #     self.assertTrue(user_stories_jp.birth_Before_Death_Of_Parents(individuals_pass, families_pass))
+    #     individuals_fail = parse_indi(fail_lines)
+    #     families_fail = parse_fam(fail_lines)
+    #     self.assertFalse(birth_Before_Death_Of_Parents(individuals_fail, families_fail))
+    def test_before_current_date(self):
+        pass_lines = [line.rstrip('\n\r') for line in open("US01_PassFile.ged")]
+        fail_lines = [line.rstrip('\n\r') for line in open("US01_FailFile.ged")]
+        individuals_pass = parse_indi(pass_lines)
+        families_pass = parse_fam(pass_lines)
+        self.assertTrue(user_stories_jp.dates_Before_Current(individuals_pass, families_pass))
+        individuals_fail = parse_indi(fail_lines)
+        families_fail = parse_fam(fail_lines)
+        self.assertFalse(user_stories_jp.dates_Before_Current(individuals_fail, families_fail))
+
+    def test_sibling_spacing(self):
+        pass_lines = [line.rstrip('\n\r') for line in open("US13_PassFile.ged")]
+        fail_lines = [line.rstrip('\n\r') for line in open("US13_FailFile.ged")]
+        individuals_pass = parse_indi(pass_lines)
+        families_pass = parse_fam(pass_lines)
+        self.assertTrue(user_stories_jp.sibling_Spacing(individuals_pass, families_pass))
+        individuals_fail = parse_indi(fail_lines)
+        families_fail = parse_fam(fail_lines)
+        self.assertFalse(user_stories_jp.sibling_Spacing(individuals_fail, families_fail))
+
+if __name__ == '__main__':
+    unittest.main()
